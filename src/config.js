a4acd51c688bdd744f01b062e066a5a1a98f7dd4
vnys <vnys@users.noreply.github.com>
2020-10-21 07:48:17 +0000

    🎨 Format code

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-const pluginPWA = require("eleventy-plugin-pwa");
+const pluginPWA = require('eleventy-plugin-pwa')
 
-module.exports = eleventyConfig => {
-    // Copy our static assets to the output folder
-    eleventyConfig.addPassthroughCopy('src/manifest.json');
-    eleventyConfig.addPassthroughCopy('src/assets/images');
-    eleventyConfig.addPlugin(pluginPWA);  
-  };
+module.exports = (eleventyConfig) => {
+  // Copy our static assets to the output folder
+  eleventyConfig.addPassthroughCopy('src/manifest.json')
+  eleventyConfig.addPassthroughCopy('src/assets/images')
+  eleventyConfig.addPlugin(pluginPWA)
+}
